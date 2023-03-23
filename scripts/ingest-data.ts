5600f9ce835200a3a8c5dc3d69678916d3cd7a0b
Roger Zhuang <zhuangziyin@gmail.com>
2023-03-22 21:23:33 -0700

    customized local case

--- scripts/ingest-data.ts
+++ scripts/ingest-data.ts
@@ -6,7 +6,8 @@ import { PDFLoader } from 'langchain/document_loaders';
 import { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE } from '@/config/pinecone';
 
 /* Name of directory to retrieve files from. You can change this as required */
-const filePath = 'docs/MorseVsFrederick.pdf';
+const filePath =
+  'docs/China Emerging Compounders Fund Limited_PPM Dec 2022.pdf';
 
 export const run = async () => {
   try {
